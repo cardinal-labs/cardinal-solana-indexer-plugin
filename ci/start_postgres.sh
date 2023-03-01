5f0bc8dcef13a2176f05d33b7e576585ac1d8568
jpbogle <jpbogle22@gmail.com>
2023-03-01 02:18:34 -0500

    Remove schema creation

--- ci/start_postgres.sh
+++ ci/start_postgres.sh
@@ -2,4 +2,4 @@
 
 set -e
 sudo /etc/init.d/postgresql start
-PGPASSWORD=solana psql -U solana -p 5432 -h localhost -w -d solana -f scripts/create_schema.sql
+PGPASSWORD=solana psql -U solana -p 5432 -h localhost -w -d solana
